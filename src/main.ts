b4499ecc1167b4effc80844a5169b952d78be8dc
Thirion Lucas <lucasthirion@live.fr>
2021-11-18 18:01:15 +0100

    V3

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import {IGrille, Grille} from './Grille.js';
+// import {IPoint, Point} from "./Point.js"
+// import {IView3D, View3D} from "./View3D.js"
+
 function init() {
   const mapCanvas: HTMLCanvasElement = document.getElementById(
     'map',
@@ -6,79 +10,70 @@ function init() {
     'view',
   ) as HTMLCanvasElement;
 
-  console.log(mapCanvas);
-  console.log(viewCanvas);
-}
+  window.addEventListener('keyup', (evt: KeyboardEvent) => {
+    switch (evt.key) {
+      case 'ArrowUp':
+        console.log('up');
+        break;
+      case 'ArrowDown':
+        console.log('down');
+        break;
+      case 'ArrowLeft':
+        console.log('left');
+        break;
+      case 'ArrowRight':
+        console.log('right');
+        break;
+    }
+    //evt.preventDefault()
+  });
+  // window.addEventListener("keydown", (evt: KeyboardEvent) => {
+  //     switch (evt.key) {
+  //         case "ArrowUp": console.log("up");break;
+  //         case "ArrowDown": console.log("down");break;
+  //         case "ArrowLeft": console.log("left");break;
+  //         case "ArrowRight": console.log("right");break;
+  //     }
+  //     //evt.preventDefault()
+  // })
 
-window.addEventListener('keyup', (evt: KeyboardEvent) => {
-  switch (evt.key) {
-    case 'ArrowUp':
-      point.deltaAvance = 0;
-      break;
-    case 'ArrowDown':
-      point.deltaRecule = 0;
-      break;
-    case 'ArrowLeft':
-      point.deltaPivotGauche = 0;
-      break;
-    case 'ArrowRight':
-      point.deltaPivotDroite = 0;
-      break;
-  }
-  //evt.preventDefault()
-});
-window.addEventListener('keydown', (evt: KeyboardEvent) => {
-  switch (evt.key) {
-    case 'ArrowUp':
-      point.deltaAvance = 1;
-      break;
-    case 'ArrowDown':
-      point.deltaRecule = 1;
-      break;
-    case 'ArrowLeft':
-      point.deltaPivotGauche = 1;
-      break;
-    case 'ArrowRight':
-      point.deltaPivotDroite = 1;
-      break;
-  }
-  //evt.preventDefault()
-});
-
-const delay: number = Math.floor(1000 / 30);
-let lastTime: number = Date.now();
-function render() {
-  const newTime: number = Date.now();
-  const delay: number = newTime - lastTime;
-  /*point.deplace(delay)
+  const delay: number = Math.floor(1000 / 30);
+  let lastTime: number = Date.now();
+  function render() {
+    const newTime: number = Date.now();
+    const delay: number = newTime - lastTime;
+    /*point.deplace(delay)
         grille.lanceRayons( point.x, point.y, point.angle )
         grille.dessine()
         point.dessine()
         view3D.dessine()
         */
-  //console.log(delay)
-  lastTime = newTime;
+    //console.log(delay)
+    lastTime = newTime;
+  }
+  setInterval(render, delay);
+  const blockStyles = {0: 'rgba(0,0,0,0)', 1: 'rgba(238,68,0,1.0)'};
+  const grille: IGrille = new Grille({
+    canvas: mapCanvas,
+    data: [
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+      [1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ],
+    blockStyles,
+    couleurFond: '#EEE',
+    couleurGrille: '#333',
+    nbRayons: viewCanvas.width,
+    angleRayons: Math.PI * 0.5,
+    // vue: view3D
+  });
+  grille.dessine();
 }
-setInterval(render, delay);
-
-const grille: IGrille = new Grille({
-  canvas: mapCanvas,
-  data: [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  ],
-  blockStyles,
-  couleurFond: '#EEE',
-  couleurGrille: '#333',
-  nbRayons: viewCanvas.width,
-  angleRayons: Math.PI * 0.5,
-  vue: view3D,
-});
+init();
